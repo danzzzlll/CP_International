af97df3faaae42fdaec993b329a5f29a14e75045
Nikita Rusakov <rusakov0609@gmail.com>
2024-11-09 21:03:26 +0300

    add new chart with task status distribuiton

--- st_web_ui/analytics.py
+++ st_web_ui/analytics.py
@@ -10,7 +10,7 @@ from utils import generate_pdf_and_download
 
 def analytics_page():
     header(True)
-    df = pd.read_csv('data/data_date.csv')
+    df = pd.read_csv('data/date_status_data.csv')
     st.markdown(f"**Суммарное кол-во записей:** {len(df)}")
     color_discrete_sequence = get_color_discrete_sequence()
     
@@ -46,18 +46,44 @@ def analytics_page():
 
         day_count = df_filtered.groupby(df_filtered['created_at']).size().reset_index(name='Количество')
         day_count = day_count.sort_values('created_at')
-        fig3 = px.line(day_count, x='created_at', y='Количество', title='Кол-во обращений в течение времени', color_discrete_sequence=color_discrete_sequence)
+        fig3 = px.line(
+            day_count,
+            x='created_at',
+            y='Количество',
+            title='Кол-во обращений в течение времени',
+            color_discrete_sequence=color_discrete_sequence
+        )
         fig3.update_xaxes(type='category')
         fig3.update_layout(xaxis_title='Дата', yaxis_title='Кол-во')
         st.plotly_chart(fig3)
 
-        day_point_count = df_filtered.groupby([df_filtered['created_at'], 'Тип оборудования']).size().reset_index(name='Количество')
-        fig4 = px.line(day_point_count, x='created_at', y='Количество', color='Тип оборудования', title='Кол-во обращений по типу обращения', color_discrete_sequence=color_discrete_sequence)
+        day_point_count = df_filtered.groupby(
+            [df_filtered['created_at'],
+             'Тип оборудования']
+            ).size().reset_index(name='Количество')
+        fig4 = px.line(
+            day_point_count,
+            x='created_at',
+            y='Количество',
+            color='Тип оборудования',
+            title='Кол-во обращений по типу обращения',
+            color_discrete_sequence=color_discrete_sequence
+        )
         fig4.update_xaxes(type='category')
         fig4.update_layout(xaxis_title='Дата', yaxis_title='Кол-во', legend_title='Тип оборудования')
         st.plotly_chart(fig4)
 
-        plots = [fig2, fig1, fig3, fig4]
+        topic_count = df_filtered['status'].value_counts().reset_index().sample(frac=1)
+        topic_count.columns = ['Статус обращения', 'Количество']
+        fig5 = px.bar(
+            topic_count,
+            x='Статус обращения',
+            y='Количество',
+            title='Статусы задач',
+            color_discrete_sequence=color_discrete_sequence
+        )
+        st.plotly_chart(fig5)
+        plots = [fig2, fig1, fig3, fig4, fig5]
 
         if st.button("Сформировать отчет"):
             pdf_buffer = generate_pdf_and_download(plots)
--- st_web_ui/streamlit_app.py
+++ st_web_ui/streamlit_app.py
@@ -2,12 +2,44 @@ import streamlit as st
 from create_appeal import create_appeal
 from analytics import analytics_page
 
+
+
+import pandas as pd
+import streamlit as st
+from st_styles.base import header
+
+def appeals():
+    header(True)
+    df = pd.read_csv('data/date_status_data.csv')
+    tasks_statuses = ["To Do", "In Progress", "Done", "Reopened", "On Hold"]
+
+    selected_statuses = st.multiselect(
+        "Select task status to filter by:",
+        options=tasks_statuses,
+        default=tasks_statuses
+    )
+    filtered_df = df[df['status'].isin(selected_statuses)]
+    st.dataframe(filtered_df, width=1000, height=600)
+
 def main():
-    page = st.sidebar.selectbox("Выберите страницу", ["Завести обращение", "Аналитика"])
+    sidebar_title = """
+        <head>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+        </head>
+        <div style="text-align: center; font-family: 'Roboto', sans-serif; padding-bottom: 30px;">
+            <span style="font-size: 44px; font-weight: bold; background: linear-gradient(90deg, #32CD32, #00FF7F); -webkit-background-clip: text; color: transparent;">Грин</span>
+            <span style="font-size: 44px; font-weight: bold; background: linear-gradient(90deg, #1E90FF, #00BFFF); -webkit-background-clip: text; color: transparent;"> МИСИС</span>
+        </div>
+    """
+
+    st.sidebar.markdown(sidebar_title, unsafe_allow_html=True)
+    page = st.sidebar.selectbox("Выберите страницу", ["Завести обращение", "Аналитика", "Обращения"])
     if page == "Завести обращение":
         create_appeal()
     elif page == "Аналитика":
         analytics_page()
+    elif page == "Обращения":
+        appeals()
 
 if __name__ == "__main__":
     main()
